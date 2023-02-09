bcec141ab6ccc512b514e0bcca6652bfa6627632
Tyler Coats <coatsctyler@gmail.com>
2023-02-09 17:21:25 -0500

    fixed controller getAllUsers

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const User = require('../models/user')
 const Country = require('../models/country')
-const { error } = require('console')
+
 
 const getAllUsers = async (req,res) => {
     try {
-        const users = await new User.find()
+        const users = await User.find()
         return res.status(200).json({users})
     } catch (error) {
         return res.status(500).send(error.message)
