212c6b030e0e2bf07ee8192e8dba14db1906ae00
Tyler Coats <coatsctyler@gmail.com>
2023-02-12 16:40:11 -0500

    Added many dashboard divs and styling

--- client/trip-planner/src/pages/Dashboard.jsx
+++ client/trip-planner/src/pages/Dashboard.jsx
@@ -1,18 +1,41 @@
 import NavBar from "../components/NavBar"
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 
-const Dashboard = ({setIsLoggedIn}) => {
+const Dashboard = ({ setIsLoggedIn }) => {
   useEffect(() => {
     setIsLoggedIn(true)
 
   }, [])
-  
-    return (
-    <div className="dashboard" style={{width: '90vw', height: '85vh'}}>
-      <h1>hello</h1>
-        <div className="outerDashboard" style={{backgroundColor: 'orange', width: '40%', height: '80%'}}>
-          nice
+
+  return (
+    <div className="dashboard" style={{}}>
+      <div className="dashboardTitle">
+        <h1>Welcome User! Let's plan your adventure today</h1>
+      </div>
+      <div className="outerDashboardLeft" style={{}}>
+        <div className="dashboardLeftTitle">
+          left side
         </div>
+        <div className="dashboardLeftTiles">
+          <div className="leftSectionTileMap">
+            <div className="mapTile">
+              Map
+              </div>
+            <div className="mapInfo">
+              info
+            </div>
+          </div>
+          <section className="leftSectionTiles"></section>
+          <section className="leftSectionTiles"></section>
+        </div>
+      </div>
+      <div className="outerDashboardRight">
+        <div className="dashboardRightTitle">
+          right side
+        </div>
+      </div>
+
+
     </div>
   )
 }
