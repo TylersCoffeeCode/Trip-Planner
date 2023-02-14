ffd34a6ad360cdb330dd2525ea26c952dbda017e
Tyler Coats <coatsctyler@gmail.com>
2023-02-14 18:53:34 -0500

    Added the start of the planner form and added 4 inputs

--- client/trip-planner/src/pages/CreateVacation.jsx
+++ client/trip-planner/src/pages/CreateVacation.jsx
@@ -21,7 +21,50 @@ const CreateVacation = ({setIsLoggedIn}) => {
 
   
     return (
-    <div className="createVacationDiv"></div>
+    <div className="createVacationDiv">
+        <div className="pageDiv">
+            <form className="vacationForm" onSubmit={''}>
+                <h1>Horizon Planner</h1>
+                <label>
+                    Where do you want to travel to?
+                    <br />
+                    <input 
+                    type='text'
+                    value={travelLocation}
+                    
+                    />
+                </label>
+                <h2 style={{fontSize: '20px'}}>What are three things you wish to experience once you're there</h2>
+                <label>
+                    Activity 1:
+                    <br />
+                    <input 
+                    type='text'
+                    value={questionOne}
+                    />
+                </label>
+                <br />
+                <label>
+                    Activity 2:
+                    <br />
+                    <input 
+                    type='text'
+                    value={questionTwo}
+                    />
+                </label>
+                <br />
+                <label>
+                    Activity 3:
+                    <br />
+                    <input 
+                    type='text'
+                    value={questionThree}
+                    
+                    />
+                </label>
+            </form>
+        </div>
+    </div>
   )
 }
 export default CreateVacation
